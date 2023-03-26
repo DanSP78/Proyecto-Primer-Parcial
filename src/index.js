3ad8a76182ecd95d293f00ebef6c518204da928f
Geraldine Torres <gerathz.08@gmail.com>
2023-03-26 01:13:35 -0600

    Proyecto Primer Parcial

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import app from "./app"; //equivale al codigo que esta en app.js
 import './database';
 import { PORT } from "./config";
+const puerto = process.env.PORT || PORT;
 
-app.listen(PORT);
-console.log("servidor en puerto", PORT);
+app.listen(puerto);
+console.log("servidor en puerto", puerto);
